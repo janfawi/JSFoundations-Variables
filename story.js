9e85b283be2287f0551d8a8f5517dcc27cebc549
janfawi <jenfawi@gmail.com>
2021-08-29 23:40:21 +0300

    Create story.js

---
+++ story.js
@@ -0,0 +1,8 @@
+let NAME = "Mshary";
+let age = 25;
+let HOBBY = "Meditation";
+
+console.log(`Mr./Mrs. ${NAME}, in 33 years, you will be ${
+  age + 33
+} years old. By then you'll have decades of ${HOBBY} practice!
+`);
